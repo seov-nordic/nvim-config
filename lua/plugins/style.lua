52ade8ddffd1ea20acf94df5389a55f04fc9dc09
Sergei Ovchinnikov <sergei.ovchinnikov@nordicsemi.no>
2024-10-17 14:13:41 +0200

    minimal init in Lua, switch to Lazy for plugin management

---
+++ lua/plugins/style.lua
@@ -0,0 +1,34 @@
+return {
+  -- the colorscheme should be available when starting Neovim
+  {
+    "folke/tokyonight.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    opts = {
+      transparent = true,
+      style = 'moon',
+      styles = {
+        sidebars = 'transparent',
+        floats = 'transparent',
+      },
+      on_highlights = function(hl, c)
+        hl.LineNr = { fg = c.fg_dark }
+        hl.LineNrAbove = { fg = c.fg_dark }
+        hl.LineNrBelow = { fg = c.fg_dark }
+        hl.CursorLineNr.bg = hl.CursorLine.bg
+      end,
+    },
+    init = function()
+      vim.cmd.colorscheme 'tokyonight-moon'
+    end
+  },
+
+  {
+    "norcalli/nvim-colorizer.lua",
+    lazy = true,
+    keys = {
+      { "<leader>cl", "<cmd>ColorizerAttachToBuffer<cr>", desc = "Colorizer" },
+    },
+    opts = {}
+  }
+}
---
+++ lua/vim-options.lua
@@ -0,0 +1,13 @@
+local o = vim.o
+local wo = vim.wo
+
+o.number = true
+o.relativenumber = true
+o.cursorline = true
+
+o.tabstop = 4
+o.softtabstop = 4
+o.shiftwidth = 4
+o.expandtab = true
+
+wo.colorcolumn = 121
